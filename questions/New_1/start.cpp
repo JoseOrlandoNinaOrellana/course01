00fc4a6c61cc04caa20d1be7b5834c9054eb8f55
José Orlando Nina Orellana <alu0101322308@ull.edu.es>
2024-03-14 22:34:55 +0000

    refactor: edit question.html and add start.cpp

---
+++ questions/New_1/start.cpp
@@ -0,0 +1,6 @@
+#include <iostream>   // std::cout std::cerr std::endl
+#include <fstream>    // std::ifstream
+#include <vector>     // std::vector<>
+
+// Returns the number of elements greater than the pivot in the vector
+int countGreater(const std::vector<int>& data, int pivot = 10) {}
